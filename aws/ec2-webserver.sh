30b11346e8deaf4566e4458643c6321da34d7f21
Daniel Blundell <hello@danblundell.com>
2017-01-15 16:27:24 +0000

    add optional region flag for s3 import

--- aws/ec2-webserver.sh
+++ aws/ec2-webserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum install httpd24 mod24_ssl php56 php56-mysqlnd -y
 yum update -y
-aws s3 cp s3://BUCKETNAMEHERE/index.html /var/www/html/
+aws s3 cp s3://BUCKETNAMEHERE/index.html /var/www/html/ #--region eu-west-1
 service httpd start
 chkconfig httpd on
